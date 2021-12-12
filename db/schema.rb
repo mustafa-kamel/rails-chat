5f55ab9945faf4a1d6b37a3bde22b91ca7c8a6e1
Mustafa Kamel <mostafak252@gmail.com>
2021-12-12 23:50:20 +0200

    Add schema

---
+++ db/schema.rb
@@ -0,0 +1,49 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_12_12_211854) do
+
+  create_table "applications", force: :cascade do |t|
+    t.string "token"
+    t.string "name"
+    t.integer "chats_count", default: 0
+    t.integer "lock_version", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["token"], name: "index_applications_on_token", unique: true
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.integer "application_id"
+    t.integer "number"
+    t.integer "messages_count", default: 0
+    t.integer "lock_version", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["application_id"], name: "index_chats_on_application_id"
+    t.index ["number", "application_id"], name: "index_chats_on_number_and_application_id", unique: true
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "chat_id"
+    t.integer "number"
+    t.string "body"
+    t.integer "lock_version", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+    t.index ["number", "chat_id"], name: "index_messages_on_number_and_chat_id", unique: true
+  end
+
+  add_foreign_key "chats", "applications"
+  add_foreign_key "messages", "chats"
+end
