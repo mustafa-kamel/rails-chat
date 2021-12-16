754ce8430a89a487ae2e6826c689ad559054e80d
Mustafa Kamel <mostafak252@gmail.com>
2021-12-16 02:16:04 +0200

    Add meassages search route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :applications, only: %i[index show create] do
     resources :chats, only: %i[index show create] do
       resources :messages, only: %i[index show create]
+      get '/search/:query', to: 'messages#search'
     end
   end
 end
